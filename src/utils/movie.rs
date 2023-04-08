7e0325c30b667dba563bc55f97ff10c3e273eb64
Ori Mizrahi <ori.mizrahi1000@gmail.com>
2023-04-08 21:13:16 +0300

    Added some stuff to debug stil have no clue

--- src/utils/movie.rs
+++ src/utils/movie.rs
@@ -17,12 +17,19 @@ impl Movie {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Watch {
+    #[serde(rename = "480")]
+    url: String,
+}
+
 // The good response we want
 #[derive(Serialize, Deserialize, Debug)]
 struct Response {
     success: bool,
-    watch: HashMap<usize, String>,
+    watch: Watch,
 }
+
 
 impl fmt::Display for Movie {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -40,18 +47,19 @@ pub async fn download(movie: &Movie) -> Result<(), Box<dyn std::error::Error>> {
     // Need to sleep cuz sratim.tv stuff
     println!("Sleeping for 30 sec");
     sleep(Duration::from_secs(30)).await;
+    println!("Finished waiting");
 
     let req = reqwest::get(format!("https://api.sratim.tv/movie/watch/id/{}/token/{}", movie.id, pre_watch))
         .await?;
 
     let body: Response = match req.json().await {
         Ok(body) => body,
-        Err(_) => {
-            panic!("Servers are full");
+        Err(e) => {
+            panic!("{}", e);
         }
     };
 
-    let movie_link = body.watch.get(&480);
+    let movie_link = body.watch.url;
     let seret = reqwest::get(format!("https:{:?}", movie_link)).await?.text().await?;
     let mut out = File::create(format!("{}.mp4", movie.name)).expect("Failed to create file");
     io::copy(&mut seret.as_bytes(), &mut out).expect("Failed to download file");
